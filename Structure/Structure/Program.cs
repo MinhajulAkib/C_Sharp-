61d51ebc65608256f07b577ad7abdeac5a896be1
Minhajul_Islam <00akibswe@gmail.com>
2021-04-23 21:51:51 +0600

    Structure in c-sharp

---
+++ Structure/Structure/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Program
+{
+	public static void Main()
+	{
+		Coordinate point;
+		//Console.Write(point.x); // Compile time error  
+
+		point.x = 10;
+		point.y = 20;
+		Console.Write(point.x); //output: 10  
+		Console.Write(point.y);
+	}
+}
+
+struct Coordinate
+{
+	public int x;
+	public int y;
+}
