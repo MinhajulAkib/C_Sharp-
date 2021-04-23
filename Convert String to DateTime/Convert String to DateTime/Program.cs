e183df7e0c9640ea5222e153ab0a3b521804e25c
Minhajul_Islam <00akibswe@gmail.com>
2021-04-23 21:48:53 +0600

    Convert string to dateTime

---
+++ Convert String to DateTime/Convert String to DateTime/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Convert_String_to_DateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var str = "5/12/2021";
+            DateTime dt;
+            var isValidDate = DateTime.TryParse(str, out dt);
+            if (isValidDate)
+                Console.WriteLine(dt);
+            else
+                Console.WriteLine($"{str} is not valid date string");
+        }
+    }
+}
