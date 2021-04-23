db8e7ed2d4d7a0844d5dd10264e0adfed5f34b14
Minhajul_Islam <00akibswe@gmail.com>
2021-04-23 21:47:26 +0600

    Array in C#

---
+++ Array/Array/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] evenNums = new int[5];
+            evenNums[0] = 2;
+            evenNums[1] = 4;
+            //evenNums[6] = 12;  //Throws run-time exception IndexOutOfRange
+
+            Console.WriteLine(evenNums[0]);  //prints 2
+            Console.WriteLine(evenNums[1]);
+        }
+    }
+}
