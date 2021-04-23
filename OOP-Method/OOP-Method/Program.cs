2bcadc0f468647e9a7cfdaad939e3993587eead2
Minhajul_Islam <00akibswe@gmail.com>
2021-04-23 21:55:58 +0600

    OOP-Method in C#

---
+++ OOP-Method/OOP-Method/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace OOP_Method
+{
+    class Program
+    {
+        static void MyMethod(string fname, int age)
+        {
+            Console.WriteLine(fname + " is " + age);
+        }
+        static void Main(string[] args)
+        {
+            MyMethod("Liam", 5);
+            MyMethod("Jenny", 8);
+            MyMethod("Anja", 31);
+        }
+    }
+}
