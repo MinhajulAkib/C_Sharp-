915bedb347dddfdf3720c4175085cb793d0cd00b
Minhajul_Islam <00akibswe@gmail.com>
2021-04-23 21:53:08 +0600

    Namespaces

---
+++ Namespaces/Namespaces/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using first_space;
+using second_space;
+
+namespace first_space{
+    class xyz{
+        public void function(){
+            Console.WriteLine("Inside first_space");
+        }
+    }
+}
+namespace second_space{
+    class dfgh{
+        public void function(){
+            Console.WriteLine("Inside second_space");
+        }
+    }
+}
+class Testclass{
+     static void Main(string[] args){
+         xyz fc = new xyz();
+         dfgh sc = new dfgh();
+         fc.function();
+         sc.function();
+         Console.ReadKey();
+    }
+}
